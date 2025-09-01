c4bdd1dc6652629630f111a8025cc8d99ebe5bb8
tymofiishulha <timoha22.a11@gmail.com>
2025-09-01 13:03:47 +0300

    First release

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -3,7 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {StorageService} from "./storage.service";
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://147.93.127.39:8084/api/auth/';
+// const AUTH_API = 'http://localhost:8080/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
